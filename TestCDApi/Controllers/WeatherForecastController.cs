82f5c6d4167ebb01da7294324f91ffd3addc4305
Ignazio Catanzaro <i.catanzaro@ocketcode.it>
2019-11-02 15:35:35 +0100

    Dockerfile 8

--- TestCDApi/Controllers/WeatherForecastController.cs
+++ TestCDApi/Controllers/WeatherForecastController.cs
@@ -9,31 +9,17 @@ namespace TestCDApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class DefaultController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        public DefaultController()
         {
-            "Freezing123", "Bracing123", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
+            
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
+            return "Ciao Randazzo";
         }
     }
 }
