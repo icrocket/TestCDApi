683eb48b9f6be2a9ee665de85908dd5d878ca890
Ignazio Catanzaro <i.catanzaro@ocketcode.it>
2019-11-02 15:48:10 +0100

    Dockerfile 9

--- TestCDApi/Controllers/WeatherForecastController.cs
+++ TestCDApi/Controllers/WeatherForecastController.cs
@@ -19,7 +19,7 @@ namespace TestCDApi.Controllers
         [HttpGet]
         public string Get()
         {
-            return "Ciao Randazzo";
+            return "Ciao Randazzo 1";
         }
     }
 }
